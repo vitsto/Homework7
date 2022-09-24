28645d94f25836cc3090506c7cbb761958efa804
vitsto <stolyarov.vitalij@list.ru>
2022-09-24 07:31:24 +0300

    initial commit

---
+++ src/Ex1.java
@@ -0,0 +1,33 @@
+public class Ex1 {
+    public static void main(String[] args) {
+        //block 1
+        //exercise 2
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+        for (i--; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        //exercise 3
+        int population = 12_000_000;
+        // рождаеость 17 человек на 1000, смертность 8 человек
+        // численность через 10 лет?
+        // год ... численность населения составляет ....
+        for (int year = 2020; year < 2030; year++) {
+            int bornPeople = 0;
+            int diedPeople = 0;
+            for (i = 1000; i <= population; i += 1000) {
+                bornPeople += 17;
+                diedPeople += 8;
+            }
+            population = population + bornPeople - diedPeople;
+            System.out.println("Год " + year + " численность населения составляет " + population);
+        }
+
+    }
+}
